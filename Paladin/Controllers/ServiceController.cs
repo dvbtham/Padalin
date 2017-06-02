70ce1619c1cfa02c79a57d66098bc66d3a3e610e
Thâm Davies <thamdv96@gmail.com>
2017-06-02 14:53:50 +0700

    working with action result.

---
+++ Paladin/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Paladin.Models;
+using Paladin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using Paladin.Infrastructure;
+
+namespace Paladin.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private PaladinDbContext _context;
+
+        public ServiceController()
+        {
+            _context = new PaladinDbContext();
+        }
+
+        public ActionResult GetApplicantsForReminders()
+        {
+            var applicants = _context.Applicants.ToList();
+            var vmApplicants = new List<ApplicantVM>();
+            foreach(var app in applicants)
+            {
+                vmApplicants.Add(Mapper.Map<ApplicantVM>(app));
+            }
+            return new XmlResult(vmApplicants);
+        }
+
+    }
+}
---
+++ Paladin/Infrastructure/XmlResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace Paladin.Infrastructure
+{
+    public class XmlResult : ActionResult
+    {
+        private readonly object _data;
+        public XmlResult(object data)
+        {
+            _data = data;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var serializer = new XmlSerializer(_data.GetType());
+            var response = HttpContext.Current.Response;
+            response.ContentType = "text/xml";
+            serializer.Serialize(response.Output, _data);
+        }
+    }
+    
+}
